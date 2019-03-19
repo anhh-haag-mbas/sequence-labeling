86b053baeb9a9c9900894430b78eff5ded125c35
Frederik Haagensen <frehaa@hotmail.dk>
2019-03-19 23:05:28 +0100

    Minor time refactor

--- code/dynet/denmark/helper.py
+++ code/dynet/denmark/helper.py
@@ -1,13 +1,7 @@
 import timeit
 
 def time(func, *args):
-    a = []
-    for e in args:
-        a.append(e)
-    l = len(a)
     start_time = timeit.default_timer()
     result = func(*args)
     elapsed = timeit.default_timer() - start_time
     return (result, elapsed)
-
-
