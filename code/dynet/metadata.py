605437c72650a2b7a7be953c36a87438c58d6706
frehaa <frehaa@hotmail.dk>
2019-04-02 21:09:07 +0200

    Generalize metadata extraction

--- code/dynet/metadata.py
+++ code/dynet/metadata.py
@@ -1,12 +1,18 @@
-from extractor import read_conllu_metadata
+from extractor import read_conllu_metadata, read_bio_metadata
 
 root_path = "../../data"
-languages = ["da", "sk"]
-files = ["training.conllu", "testing.conllu", "validation.conllu"]
-tasks = ["pos"]
+languages = ["ar", "de", "el", "en", "fr", "hu", "ja", "ko", "ru", "sk", "tr"]
+files = ["training", "testing", "validation"]
+tasks = ["ner"]
 
-for lang in languages:
+for lang in languages[0:1]:
     for f in files:
         for task in tasks:
-            print(f"{lang}\t{task}\t{f}\t{read_conllu_metadata(f'{root_path}/{task}/{lang}/{f}')}")
-        
+            data = ""
+            if task == "ner":
+                data = read_bio_metadata(f'{root_path}/{task}/{lang}/{f}.bio')
+            elif task == "pos":
+                data = read_conllu_metadata(f'{root_path}/{task}/{lang}/{f}.conllu')
+            else:
+                raise ValueError("Unknown task")
+            print(f"{lang}\t{task}\t{f}\t{data}")
