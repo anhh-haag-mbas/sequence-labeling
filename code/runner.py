e5a356b8d810032aeef1aa616c88b3de8a519b26
haag <haag@itu.dk>
2019-04-05 17:24:07 +0200

    Create runners main loop

--- code/runner.py
+++ code/runner.py
@@ -9,6 +9,16 @@ seeds = [613321, 5123, 421213, 521403, 322233]
 batch_sizes = [1, 8, 32]
 epochs = [1, 5, "patience-3"]
 
+parameters = {
+        "framwork": framework,
+        "languages": languages,
+        "tasks": tasks,
+        "crf": crf,
+        "seeds": seeds,
+        "batch_sizes": batch_sizes,
+        "epochs": epochs
+        }
+
 def config_to_str(config, separator):
     values = []
     keys = ["framework", "language", "task", "crf", "seed", "batch_size", "epochs"]
@@ -18,17 +28,48 @@ def config_to_str(config, separator):
 def results_to_str(results, separator):
     return ""
 
-config = {
-        "framework": "dynet",
-        "language": "en",
-        "task": "pos",
-        "crf": True,
-        "seed": 5124,
-        "batch_size": 8,
-        "epochs": 5
-        }
+def run_experiment(config):
+    framework = config["framework"]
+    if framework == "dynet":
+        return run_experiment_dynet(config)
+    if framework == "pytorch":
+        return run_experiment_pytorch(config)
+    if framework == "tensorflow":
+        return run_experiment_tensorflow(config)
 
-print(config_to_str(config, ","))
+def run_experiment_dynet(config):
+    pass
 
-#with open("out.csv", "w", encoding = "utf-8") as of:
-#    of.write("Hello")
+def run_experiment_pytorch(config):
+    pass
+
+def run_experiment_tensorflow(config):
+    pass
+
+def configuration_count(parameters):
+    lengths = [len(parameters[k]) for k in parameters]
+    total = 1
+    for l in lengths:
+        total *= l
+    return total
+
+count = 0
+total = configuration_count(parameters)
+for framework, seed, batch_size, epoch, task, model, language \
+                in zip(epochs, seeds, batch_sizes, epochs, tasks, crf, languages):
+        config = {
+                "framework": framework,
+                "language": language,
+                "task": task,
+                "crf": model,
+                "seed": seed,
+                "batch_size": batch_size,
+                "epochs": epoch
+                }
+        results = run_experiment(config)
+        count += 1
+        print(f"{count} / {total} - {config_to_str(config, ' ')} ", file=sys.stderr)
+#        with open("out.csv", "w", encoding = "utf-8") as of:
+#            of.write(config_to_str(config,separator)+separator+results_to_str(results, separator))
+
+
