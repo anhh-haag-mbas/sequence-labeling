080d15e521a2ba858e49b4f79eb2a251d8813c45
haag <haag@itu.dk>
2019-04-10 21:17:43 +0200

    Add missing runner features

--- code/runner.py
+++ code/runner.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from itertools import product
-import dynet_sequence_labeling.dynet_sequence_labeling
+import dynet_sequence_labeling.dynet_sequence_labeling as dysl
 from polyglot.mapping import Embedding
 
 def config_to_str(config, separator):
@@ -10,20 +10,26 @@ def config_to_str(config, separator):
     return separator.join(values)
 
 def evaluation_to_str(evaluation, separator):
-    return ""
+    keys = list(evaluation.keys())
+    keys.sort()
+    values = []
+    for expected, actual in product(keys, keys):
+        values.append(str(evaluation[expected][actual]))
+
+    return separator.join(values)
 
 def results_to_str(results, separator):
     keys = ["total_values", "total_errors", "total_oov", "total_oov_errors", "training_time", "evaluation_time", "epochs_run"]
-    values = map(lambda k: str(config[k]), keys)
+    values = map(lambda k: str(results[k]), keys)
     return separator.join(values) + evaluation_to_str(results["evaluation_matrix"], separator)
 
-def load_embeddings(data_root, languages):
-    return [Embedding.load(data_root + f"embeddings/{l}.tar.bz2") for l in languages]
+def load_embedding(data_root, languages):
+    return {l:Embedding.load(data_root + (f"embeddings/{l}.tar.bz2")) for l in languages}
 
 def run_experiment(config):
     framework = config["framework"]
     if framework == "dynet":
-        return dynet_sequence_labeling.run_experiment(config)
+        return dysl.run_experiment(config)
     if framework == "pytorch":
         return run_experiment_pytorch(config)
     if framework == "tensorflow":
@@ -37,12 +43,12 @@ def run_experiment_tensorflow(config):
 
 def experiment_to_str(config, results):
     separator = ","
-    return config_to_str(config,separator)+separator+results_to_str(results, separator)
+    return config_to_str(config,separator)+separator+results_to_str(results, separator)+"\n"
 
 frameworks = ["dynet", "pytorch", "tensorflow"]
 languages = ["de", "nl", "ja", "en", "sv", "zh", "sk", "ar", "el"]
 tasks = ["pos", "ner"]
-models = [True, False]
+models = [False, True]
 seeds = [613321, 5123, 421213, 521403, 322233]
 batch_sizes = [1, 8, 32]
 epochs = [1, 5, {"max": 50, "patience": 3}]
@@ -52,8 +58,7 @@ count = 0
 configurations = product(frameworks, seeds, batch_sizes, epochs, tasks, models, languages)
 config_count = len(frameworks) * len(seeds) * len(batch_sizes) * len(epochs) * len(tasks) * len(models) * len(languages)
 
-
-embeddings = load_embeddings(data_root, languages)
+embedding = load_embedding(data_root, languages)
 
 for framework, seed, batch_size, epoch, task, model, language in configurations:
 
@@ -67,15 +72,14 @@ for framework, seed, batch_size, epoch, task, model, language in configurations:
             "epochs": epoch if not isinstance(epoch, dict) else epoch["max"],
             "patience": None if not isinstance(epoch, dict) else epoch["patience"],
             "hidden_size": 100,
-            "dropout": 0.5,
+            "dropout": 0.5, 
+            "data_root": data_root,
+            "embedding": embedding[language],
             "optimizer": "sgd",
             "learning_rate": 0.1,
-            "data_root": data_root,
-            "embedding": polyglot_embedding
             }
     count += 1
     print(f"{count} / {config_count} - {config_to_str(config, ' ')}")
-
     results = run_experiment(config)
-    with open("out.csv", "w", encoding = "utf-8") as of:
+    with open("out.csv", "a", encoding = "utf-8") as of:
         of.write(experiment_to_str(config, results))
