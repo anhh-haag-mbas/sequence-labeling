0a2d111628e93cea96a264209ce6e470c5319ef6
haag <haag@itu.dk>
2019-04-10 11:49:21 +0200

    Add polyglot downloading to download_generator

--- data/download_generator.py
+++ data/download_generator.py
@@ -1,7 +1,5 @@
 """
 Reads the language codes in languages.txt to generate bash scripts for downloading all the appropriate NER and fasttext files
-TODO: Somehow extend with polyglot file downloads
-      http://polyglot.cs.stonybrook.edu/~polyglot/embeddings2/ja/embeddings_pkl.tar.bz2
 """
 import sys
 
@@ -13,13 +11,13 @@ with open("language_codes.txt", "r", encoding = 'utf-8') as f:
         code2lang[split[0]] = "_".join(split[1:])
 
 with open("languages.txt", "r") as f, \
-     open("embeddings/fasttext/download_files.sh", "w") as fembf, \
+     open("embeddings/download_files.sh", "w") as embf, \
      open("ner/download_files.sh", "w") as nerf:
 
     for line in f:
         code = line.strip()
         nerf.write(f"wget -nc -O ./{code}.tar.gz https://blender04.cs.rpi.edu/~panx2/wikiann/data/{code}.tar.gz\n")
-        fembf.write(f"wget -nc -O ./{code}.bin.gz https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.{code}.300.bin.gz\n")
+        embf.write(f"wget -nc -O ./{code}.tar.bz2 http://polyglot.cs.stonybrook.edu/~polyglot/embeddings2/{code}/embeddings_pkl.tar.bz2\n")
 
 with open("pos/download_all_files.sh", "w") as posf:
     posf.write('wget -nc -O ud-treebanks-v2.3.tgz "https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-2895/ud-treebanks-v2.3.tgz?sequence=1&isAllowed=y"')
