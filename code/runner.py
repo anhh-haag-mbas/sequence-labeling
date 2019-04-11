37af80495abbf3da90156d7b03b8228ae6539dac
frehaa <frehaa@hotmail.dk>
2019-04-11 15:38:25 +0200

    Fix missing value separator in runner

--- code/runner.py
+++ code/runner.py
@@ -4,6 +4,12 @@ from itertools import product
 import dynet_sequence_labeling.dynet_sequence_labeling as dysl
 from polyglot.mapping import Embedding
 
+def validate_results(results):
+    keys = ["total_values", "total_errors", "total_oov", "total_oov_errors", "training_time", "evaluation_time", "epochs_run"]
+    for key in keys:
+        if key not in results.keys():
+            raise ValueError(f"Missing value {key} from experiment results")
+
 def config_to_str(config, separator):
     keys = ["framework", "language", "task", "crf", "seed", "batch_size", "epochs", "patience"]
     values = map(lambda k: str(config[k]), keys)
@@ -21,9 +27,9 @@ def evaluation_to_str(evaluation, separator):
 def results_to_str(results, separator):
     keys = ["total_values", "total_errors", "total_oov", "total_oov_errors", "training_time", "evaluation_time", "epochs_run"]
     values = map(lambda k: str(results[k]), keys)
-    return separator.join(values) + evaluation_to_str(results["evaluation_matrix"], separator)
+    return separator.join(values) + separator + evaluation_to_str(results["evaluation_matrix"], separator)
 
-def load_embedding(data_root, languages):
+def load_embeddings(data_root, languages):
     return {l:Embedding.load(data_root + (f"embeddings/{l}.tar.bz2")) for l in languages}
 
 def run_experiment(config):
@@ -46,8 +52,12 @@ def experiment_to_str(config, results):
     return config_to_str(config,separator)+separator+results_to_str(results, separator)+"\n"
 
 frameworks = ["dynet", "pytorch", "tensorflow"]
-languages = ["de", "nl", "ja", "en", "sv", "zh", "sk", "ar", "el"]
-tasks = ["pos", "ner"]
+#languages = ["de", "nl", "ja", "en", "sv", "zh", "sk", "ar", "el"]
+#tasks = ["pos", "ner"]
+
+languages = ["nl"]
+tasks = ["pos"]
+
 models = [False, True]
 seeds = [613321, 5123, 421213, 521403, 322233]
 batch_sizes = [1, 8, 32]
@@ -58,7 +68,7 @@ count = 0
 configurations = product(frameworks, seeds, batch_sizes, epochs, tasks, models, languages)
 config_count = len(frameworks) * len(seeds) * len(batch_sizes) * len(epochs) * len(tasks) * len(models) * len(languages)
 
-embedding = load_embedding(data_root, languages)
+embeddings = load_embeddings(data_root, languages)
 
 for framework, seed, batch_size, epoch, task, model, language in configurations:
 
@@ -72,14 +82,16 @@ for framework, seed, batch_size, epoch, task, model, language in configurations:
             "epochs": epoch if not isinstance(epoch, dict) else epoch["max"],
             "patience": None if not isinstance(epoch, dict) else epoch["patience"],
             "hidden_size": 100,
-            "dropout": 0.5, 
+            "dropout": 0.5,
             "data_root": data_root,
-            "embedding": embedding[language],
+            "embedding": embeddings[language],
             "optimizer": "sgd",
             "learning_rate": 0.1,
             }
     count += 1
     print(f"{count} / {config_count} - {config_to_str(config, ' ')}")
     results = run_experiment(config)
+    validate_results(results)
+    print(results)
     with open("out.csv", "a", encoding = "utf-8") as of:
         of.write(experiment_to_str(config, results))
